20726129b496e1a4ba150bd330dc098d541ad636
liuzhuang <1271903292.@qq.com>
2017-01-23 15:45:41 +0800

    first a bbs

---
+++ MyBBs/My_BBS/app/models.py
@@ -0,0 +1,36 @@
+import sys
+sys.setdefaultencoding("utf-8") 
+
+from django.db import models
+from django.contrib.auth.models import User
+# Create my model this
+
+class BBS(models.Model):
+    category = models.ForeignKey('Category')
+    title = models.CharField(max_length=64)
+    summary = models.CharField(max_length=256,blank=True,null=True)
+    content = models.TextField()
+    author = models.ForeignKey('BBS_user')
+    view_count=  models.IntegerField()
+    ranking = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        return self.title
+    
+class Category(models.Model):
+    name= models.CharField(max_length=32,unique=True)
+    administrator = models.ForeignKey('BBS_user')
+    def __unicode__(self):
+        return self.name    
+    
+class BBS_user(models.Model):
+    user = models.OneToOneField(User)
+    signature = models.CharField(max_length=128, default='啊哈，blank')
+    photo=models.ImageField(upload_to="upload_imgs/" , default="upload_imgs/user-1.jpg") 
+    
+    def __unicode__(self):
+        return self.user.username   
+    
+    
